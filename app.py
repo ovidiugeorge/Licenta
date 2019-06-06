32b956cc841b00ebe017fa6c396db33e50520092
ovidiugeorge <ovidiu_george_paiu@yahoo.ro>
2019-06-06 15:11:35 +0300

    fisrt

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_app():
+    return render_template("index.html")
+
+@app.route('/region')
+def region():
+    return render_template("region.html")
